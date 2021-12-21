2d0c0e55dcde8ea8b23d32f771b493ef3864cfdd
Gabriel Azevedo <gabrielazevedo318@gmail.com>
2021-12-20 23:00:42 -0300

    Chapter l finalizado e aplicação de listagem dos repositórios do GitHub finalizada

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Counter } from './components/Counter'
+import { RepositoryList } from './components/RepositoryList'
+import './styles/global.scss'
+
+export function App() {
+    // throw new Error('Eita Giovana, o forninho caiu!')
+
+    return (
+        <>
+            <RepositoryList />
+        </>
+    )
+}
---
+++ src/components/RepositoryList.tsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react'
+import { RepositoryItem } from './RepositoryItem';
+import '../styles/repositories.scss'
+
+interface Repository {
+    id: number;
+    name: string;
+    description: string;
+    html_url: string;
+}
+
+export function RepositoryList() {
+    const [repositories, setRepositories] = useState<Repository[]>([])
+
+    useEffect(() => {
+        fetch('https://api.github.com/users/gabrielaz318/repos')
+        .then(res => res.json())
+        .then(data => setRepositories(data))
+    }, [])
+
+    return (
+        <section className="repository-list">
+            <h1>Lista de repositórios</h1>
+
+            <ul>
+                {repositories.map(repository => (
+                    <RepositoryItem key={repository.id} repository={repository}  />
+                ))}
+            </ul>
+        </section>
+    )
+} 
